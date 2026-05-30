98a13d8b8e5c95b78ea67aec8de9f5f479ec62fa
SotoR713 <Soto_713@hotmail.com>
2026-05-29 17:11:30 -0700

    Agregar archivos de proyecto.

---
+++ ConSot.Core/Models/Medicamento.cs
@@ -0,0 +1,19 @@
+﻿namespace ConSot.Core.Models
+{
+    public class Medicamento
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Existencia { get; set; }
+        public string Presentacion { get; set; }
+
+        public Medicamento(int id, string nombre, int existencia, string presentacion)
+        {
+            Id = id;
+            Nombre = nombre;
+            Existencia = existencia;
+            Presentacion = presentacion;
+        }
+    }
+}
+
---
+++ ConSot.Core/Models/Paciente.cs
@@ -0,0 +1,17 @@
+﻿namespace ConSot.Core.Models
+{
+    public class Paciente
+    {
+
+        public int Id { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+
+        public Paciente(int id, string nombres, string apellidos)
+        {
+            Id = id;
+            Nombres = nombres;
+            Apellidos = apellidos;
+        }
+    }
+}
